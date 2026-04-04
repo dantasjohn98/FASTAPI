9eb84c315fb3a4254840501c9fde2b9f84243f0a
John Dantas <dantasjohn@hotmail.com>
2026-04-04 19:00:26 -0300

    implementaando rotas CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,58 @@
 from http import HTTPStatus
-from fastapi.testclient import TestClient
-
-from fastapi_zero.app import app
 
 
+def test_root_deve_retornar_ola_mundo(client):
+    # esse teste tem 3 etapas (AAA):
+    # Arrange: preparar o ambiente para o teste
+    # Act: executar a ação que queremos testar
+    # Assert: verificar se o resultado é o esperado
 
-def test_root_deve_retornar_ola_mundo():
-    
-    #esse teste tem 3 etapas (AAA):
-    #Arrange: preparar o ambiente para o teste
-    #Act: executar a ação que queremos testar
-    #Assert: verificar se o resultado é o esperado
-
-    #Arrange
-    client = TestClient(app)
-    #Act
     response = client.get('/')
-    #Assert
+
     assert response.json() == {'message': 'Olá Mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_create_user(client):
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'password',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [{'username': 'alice', 'email': 'alice@example.com', 'id': 1}]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
